f43e19971af77822262790709ca244260ea91fa0
Alexis Santos <90649691+Alexis1santos@users.noreply.github.com>
2025-11-07 18:10:15 -0600

    Update proyectoFinal.cpp
    Modificación de modelo piso

--- proyectoFinal.cpp
+++ proyectoFinal.cpp
@@ -839,8 +839,8 @@ int main()
 
 		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
 		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
+		model = glm::translate(model, glm::vec3(20.0f, -2.0f, 70.0f));
+		model = glm::scale(model, glm::vec3(24.5f, 1.0f, 24.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
 		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
@@ -1317,4 +1317,5 @@ void inputKeyframes(bool* keys)
 	}
 	else reproducirAnimacion = 0;
 }
+
 
